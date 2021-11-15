e297759220470d373c19e2eb450c8c42c90874d8
Nico Pretorius <nico.pretorius@edifyq.com>
2021-11-15 22:13:00 +0000

    Initial commit

---
+++ components/layouts/default.js
@@ -0,0 +1,23 @@
+
+import Head from 'next/head';
+
+import { Grid, GridColumn, GridRow, Header, Message } from 'semantic-ui-react';
+
+export default function DefaultLayout({}) {
+    return (
+        <>
+            <Head>
+                <title>p2ploans.co.uk</title>
+            </Head>
+            <Grid columns={1}>
+                <GridRow>
+                    <GridColumn>
+                        <div style={{padding: '16px'}}>
+                            <Header as="h1" block color="grey">p2ploans.co.uk</Header>
+                        </div>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
+        </>
+    )
+}
