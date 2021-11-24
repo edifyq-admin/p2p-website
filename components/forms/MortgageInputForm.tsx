bcb1c71f8ca93e67a26fcb998725d8804c86271d
Nico Pretorius <nico.pretorius@edifyq.com>
2021-11-24 21:33:54 +0000

    Enable toggle button

--- components/forms/MortgageInputForm.tsx
+++ components/forms/MortgageInputForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Button, Form, FormField, Grid, GridColumn, GridRow, Header, Input, Label, List, Segment } from "semantic-ui-react"
+import { Button, Form, FormField, Grid, GridColumn, GridRow, Header, Input, Label, List, Radio, Segment } from "semantic-ui-react"
 import { getMortgage } from "../../services/MortgageService";
 
 export const MortgageInputForm = () => {
@@ -11,6 +11,8 @@ export const MortgageInputForm = () => {
     const [interestError, setInterestError] = useState<string>();
     const [mortgageError, setMortgageError] = useState<string>();
     const [termError, setTermError] = useState<string>();
+
+    const [ enableMulti, setEnableMulti ] = useState<boolean>(false);
 
     const handleInterestChange = (event: any) => setInterest(event.target.value);
     const handleLoanAmountChange = (event: any) => setLoanAmount(event.target.value);
@@ -52,48 +54,59 @@ export const MortgageInputForm = () => {
     return (
         <Segment raised color="blue">
             <Header as="h1" color="blue" textAlign="center">Mortgage Form</Header>
-            <Form>
-                <List relaxed size="huge">
-                    <List.Item>
-                        <FormField>
-                            <Input fluid size="mini" icon='pound sign' iconPosition='left' type="number" placeholder="Mortgage amount" value={loanAmount} onChange={handleLoanAmountChange} />
-                            {mortgageError && <Label basic color="red" pointing>
-                                {mortgageError}
-                            </Label>}
-                        </FormField>
-                    </List.Item>
-                    <List.Item>
-                        <Grid stackable>
-                            <GridRow columns={2}>
-                                <GridColumn>
-                                    <Input fluid size="mini" icon='percent' type="number" placeholder="Interest rate" value={interest} onChange={handleInterestChange} />
-                                    {interestError && <Label basic color="red" pointing>
-                                        {interestError}
-                                    </Label>}
-                                </GridColumn>
-                                <GridColumn>
-                                    <Input fluid size="mini" icon='calendar alternate outline' iconPosition="left" label={{ basic: true, content: 'Months' }} labelPosition="right" type="number" placeholder="Term" value={term} onChange={handleTermChange} />
-                                    {termError && <Label basic color="red" pointing>
-                                        {termError}
-                                    </Label>}
-                                </GridColumn>
-                            </GridRow>
-                        </Grid>
-                    </List.Item>
-                    <List.Item>
-                        <Grid columns={2} stackable>
-                            <Grid.Row>
-                                <GridColumn>
-                                    <Button fluid primary size="massive" onClick={sendData}>Calculate</Button>
-                                </GridColumn>
-                                <GridColumn>
-                                    <Button fluid secondary size="massive" onClick={clearData}>Reset</Button>
-                                </GridColumn>
-                            </Grid.Row>
-                        </Grid>
-                    </List.Item>
-                </List>
-            </Form>
+            <div>
+                <Radio
+                    toggle
+                    checked={enableMulti}
+                    label="Enable multiterm mortgage"
+                    onClick={() => setEnableMulti(!enableMulti)}
+                />
+            </div>
+            <br/>
+            {!enableMulti &&
+                <Form>
+                    <List relaxed size="huge">
+                        <List.Item>
+                            <FormField>
+                                <Input fluid size="mini" icon='pound sign' iconPosition='left' type="number" placeholder="Mortgage amount" value={loanAmount} onChange={handleLoanAmountChange} />
+                                {mortgageError && <Label basic color="red" pointing>
+                                    {mortgageError}
+                                </Label>}
+                            </FormField>
+                        </List.Item>
+                        <List.Item>
+                            <Grid stackable>
+                                <GridRow columns={2}>
+                                    <GridColumn>
+                                        <Input fluid size="mini" icon='percent' type="number" placeholder="Interest rate" value={interest} onChange={handleInterestChange} />
+                                        {interestError && <Label basic color="red" pointing>
+                                            {interestError}
+                                        </Label>}
+                                    </GridColumn>
+                                    <GridColumn>
+                                        <Input fluid size="mini" icon='calendar alternate outline' iconPosition="left" label={{ basic: true, content: 'Months' }} labelPosition="right" type="number" placeholder="Term" value={term} onChange={handleTermChange} />
+                                        {termError && <Label basic color="red" pointing>
+                                            {termError}
+                                        </Label>}
+                                    </GridColumn>
+                                </GridRow>
+                            </Grid>
+                        </List.Item>
+                        <List.Item>
+                            <Grid columns={2} stackable>
+                                <Grid.Row>
+                                    <GridColumn>
+                                        <Button fluid primary size="massive" onClick={sendData}>Calculate</Button>
+                                    </GridColumn>
+                                    <GridColumn>
+                                        <Button fluid secondary size="massive" onClick={clearData}>Reset</Button>
+                                    </GridColumn>
+                                </Grid.Row>
+                            </Grid>
+                        </List.Item>
+                    </List>
+                </Form>
+            }
         </Segment>
     )
 }
